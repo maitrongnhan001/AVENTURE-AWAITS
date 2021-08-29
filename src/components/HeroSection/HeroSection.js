ed18f971a57be1b58a8a59014a93c7d99d325e0e
maitrongnhan001 <maitrongnhan.001@gmail.com>
2021-08-29 23:43:01 +0700

    added router, navbar and video

---
+++ src/components/HeroSection/HeroSection.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import '../../App.css';
+import {Button} from '../Button/Button';
+import './HeroSection.css';
+const HeroSection = () => {
+    return (
+        <div className = 'hero-container'>
+            <video src = '/videos/video-2.mp4' autoPlay loop muted></video>
+            <h1>AVENTURE AWAITS</h1>
+            <p>Where are you wating for?</p>
+            <div className = 'hero-btns'>
+                <Button
+                    className = 'btns'
+                    buttonStyle = 'btn-outline'
+                    buttonSize = 'btn-large'
+                >
+                    GET STARTED
+                </Button>
+                <Button
+                    className = 'btns'
+                    buttonStyle = 'btn-primary'
+                    buttonSize = 'btn-large'
+                >
+                    WATCH TRAILER<i className = 'far fa-play-circle'></i>
+                </Button>
+            </div>
+        </div>
+    );
+}
+
+export default HeroSection;
