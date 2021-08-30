0f481522ddefeed6a6f3e62c6f1a2bcfe5c43bf3
maitrongnhan001 <maitrongnhan.001@gmail.com>
2021-08-30 21:13:24 +0700

    completed project

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import Navbar from './components/navbar/Navbar';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from './components/Pages/Home';
+import Services from './components/Pages/Services';
+import Products from './components/Pages/Products';
+import SignUp from './components/Pages/SignUp';
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
         <Navbar />
         <Switch>
           <Route path = '/' exact component = {Home}></Route>
+          <Route path = '/services' component = {Services}></Route>
+          <Route path = '/products' component = {Products}></Route>
+          <Route path = '/sign-Up' component = {SignUp}></Route>
         </Switch>
       </Router>
     </>
--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Cards from '../Cards/Cards';
+import Footer from '../Footer/Footer';
 import HeroSection from '../HeroSection/HeroSection';
 
 const Home = () => {
     return (
         <>
             <HeroSection></HeroSection>
+            <Cards></Cards>
+            <Footer></Footer>
         </>
     );
 }
